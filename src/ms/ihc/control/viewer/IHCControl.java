d2a5a9a74a26b0775804474e960140a77000291e
Morten Striboldt <morten@striboldt.com>
2013-08-11 18:58:24 +0200

    Migrated to new DialogFragment instead og deprecated OnCreateDialog

--- src/ms/ihc/control/viewer/IHCControl.java
+++ src/ms/ihc/control/viewer/IHCControl.java
@@ -6,27 +6,21 @@ import com.android.vending.licensing.AESObfuscator;
 import com.android.vending.licensing.LicenseChecker;
 import com.android.vending.licensing.LicenseCheckerCallback;
 import com.android.vending.licensing.ServerManagedPolicy;
+import ms.ihc.control.fragments.AlertDialogFragment;
 import ms.ihc.control.fragments.LocationFragment;
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager.NameNotFoundException;
-import android.net.ConnectivityManager;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings.Secure;
-import android.view.KeyEvent;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -35,7 +29,7 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class IHCControl extends Activity {
+public class IHCControl extends FragmentActivity {
 
 	private LicenseCheckerCallback mLicenseCheckerCallback;   
 	private LicenseChecker mChecker;
@@ -49,14 +43,16 @@ public class IHCControl extends Activity {
 	private static final String BASE64_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvXLkhAGbLsxdP9DMAmAmg14LbqSirf8zlfAV4NL1qlVkNXxqFy3OvNlhZQSiSXV/Ir2JDJy7gK1nBa2yoeQuf0jyo6iGdY/m68Vt1yrQU8ouqOcpOt8626X/XeconYf+aLg55pEXWba5Uhy8To2dd3tjMDMtW6e/EtvOyRPJPNfhK3DS5sk2o8lOCQntgVOnByUgi8iZj6Bm8VdBra2DWXyXqbvhE7m6EXmFUlezWRsjnD+XswIQyHA5DLZ0ZjZ1bwetMMFIb0Ru3dZ1cmtGIRvFoTyvAgX1XJnSFrhBo/9z0/eHTtNReIWQgnJvq+8e79G6rINdL4Xa+kKjhLQIFwIDAQAB";
 	// Generate 20 random bytes, and put them here.    
 	private static final byte[] SALT = new byte[] { -45, 35, 39, -18, -123, -117, 74, -24, 77, 123, -100, -42, 74, -122, -14, -102, -11, 0, -6, 22};
-	Button next;
-	Button previous;
+
 	private static final Boolean simulationMode = false;
-	
-	private CompoundButton autoLoginBox;
+
 	private CompoundButton reloadProjectCheckBox;
 	private CompoundButton wanCheckBox;
-	
+    private EditText wanIpEditText;
+    private EditText lanIpEditText;
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+
     private class MyLicenseCheckerCallback implements LicenseCheckerCallback {
     	
     	public void allow() 
@@ -77,43 +73,12 @@ public class IHCControl extends Activity {
     			// Don't update UI if Activity is finishing.        
     			return;        
   			}
-    		showDialog(0);
-			
+            final AlertDialogFragment dialogFragment = AlertDialogFragment.newInstance(R.string.unlicensed_dialog_title);
+			dialogFragment.show(getSupportFragmentManager(), "dialog");
     	}
 
 		public void applicationError(ApplicationErrorCode errorCode) {	
 		}   
-    }
-    
-    protected Dialog onCreateDialog(int id) {
-        // We have only one dialog.
-        return new AlertDialog.Builder(this)
-                .setTitle(R.string.unlicensed_dialog_title)
-                .setMessage(R.string.unlicensed_dialog_body)
-                .setPositiveButton(R.string.buy_button, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        Intent marketIntent = new Intent(Intent.ACTION_VIEW).setData( Uri.parse(
-                                "market://details?id=" + getPackageName()));
-                        startActivity(marketIntent);
-                        finish();
-                    }
-                })
-                .setNegativeButton(R.string.quit_button, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        finish();
-                    }
-                })
- 
-                .setCancelable(false)
-                .setOnKeyListener(new DialogInterface.OnKeyListener(){
-					public boolean onKey(DialogInterface dialog, int keyCode,
-							KeyEvent event) {
-                        finish();
-                        return true;
-					}
-                })
-                .create();
- 
     }
  
 
@@ -137,12 +102,10 @@ public class IHCControl extends Activity {
 		
 		if (Build.VERSION.SDK_INT >= 14) {
 			this.wanCheckBox = (Switch) findViewById(R.id.WanOnly);
-			//this.autoLoginBox = (Switch) findViewById(R.ihc.autologin);
 			this.reloadProjectCheckBox = (Switch) findViewById(R.id.reloadProject);
 		}
 		else {
 			this.wanCheckBox = (CheckBox) findViewById(R.id.WanOnly);
-			//this.autoLoginBox = (CheckBox) findViewById(R.ihc.autologin);
 			this.reloadProjectCheckBox = (CheckBox) findViewById(R.id.reloadProject);
 		}
 		
@@ -150,7 +113,7 @@ public class IHCControl extends Activity {
 		
 		if(!settings.getBoolean("isLicensed", false))
 		{
-			String deviceId = Secure.getString(this.getContentResolver(),Secure.ANDROID_ID);  
+			final String deviceId = Secure.getString(this.getContentResolver(),Secure.ANDROID_ID);
 			// Construct the LicenseCheckerCallback. The library calls this when done.       
 			mLicenseCheckerCallback = new MyLicenseCheckerCallback();
 			
@@ -171,41 +134,51 @@ public class IHCControl extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		Context context = this;
 
 		// Handle item selection
 		switch (item.getItemId()) {
 		case R.id.locations:
-				String ip;
 
-				String wanip = ((EditText) findViewById(R.id.wan_ip)).getText().toString();
-				String lanip = ((EditText) findViewById(R.id.lan_ip)).getText().toString();
-				if (wanCheckBox.isChecked()) {
-					ip = wanip;
-				} else {
-					ip = lanip;
-				}
+            String ip;
+            String wanip = "";
+            String lanip = "";
+
+            this.wanIpEditText = (EditText) findViewById(R.id.wan_ip);
+
+            if(this.wanIpEditText.getText() != null)
+                wanip =  this.wanIpEditText.getText().toString();
+
+
+            this.lanIpEditText = (EditText) findViewById(R.id.lan_ip);
+            if(this.lanIpEditText.getText() != null)
+                lanip = this.lanIpEditText.getText().toString();
+
+			if (this.wanCheckBox.isChecked()) {
+				ip = wanip;
+			} else {
+				ip = lanip;
+			}
+
 	
-				String username = ((EditText) findViewById(R.id.username)).getText().toString();
-				String password = ((EditText) findViewById(R.id.password)).getText().toString();
-	
-				SharedPreferences saveSettings = getSharedPreferences(PREFS_NAME, 0);
-				SharedPreferences.Editor editor = saveSettings.edit();
-				editor.putString("username", username);
-				editor.putString("password", password);
-				editor.putString("lan_ip", lanip);
-				editor.putString("wan_ip", wanip);
-				editor.putString("active_ip", ip);
-				editor.putBoolean("autologin", this.autoLoginBox.isChecked());
-				editor.putBoolean("wanonly", this.wanCheckBox.isChecked());
-				editor.putBoolean("reloadProject",this.reloadProjectCheckBox.isChecked());
-				editor.commit();
-	
-				new LoginTask().execute(username, password, ip);
+            final String username = ((EditText) findViewById(R.id.username)).getText().toString();
+            final String password = ((EditText) findViewById(R.id.password)).getText().toString();
+
+            final SharedPreferences saveSettings = getSharedPreferences(PREFS_NAME, 0);
+            final SharedPreferences.Editor editor = saveSettings.edit();
+            editor.putString("username", username);
+            editor.putString("password", password);
+            editor.putString("lan_ip", lanip);
+            editor.putString("wan_ip", wanip);
+            editor.putString("active_ip", ip);
+            editor.putBoolean("wanonly", this.wanCheckBox.isChecked());
+            editor.putBoolean("reloadProject",this.reloadProjectCheckBox.isChecked());
+            editor.commit();
+
+            new LoginTask().execute(username, password, ip);
 			return true;
 
 		case R.id.about:
-			Dialog dialog = new Dialog(context);
+			final Dialog dialog = new Dialog(this);
 			dialog.setContentView(R.layout.about);
 			dialog.setTitle(getResources().getString(R.string.about));
 			String version = "";
@@ -218,16 +191,17 @@ public class IHCControl extends Activity {
 			}
 			
 			
-			TextView email = (TextView) dialog.findViewById(R.id.email);
-			TextView author = (TextView) dialog.findViewById(R.id.author);
-			TextView text = (TextView) dialog.findViewById(R.id.text);
-			TextView versionView = (TextView) dialog.findViewById(R.id.version);
+			final TextView email = (TextView) dialog.findViewById(R.id.email);
+            final TextView author = (TextView) dialog.findViewById(R.id.author);
+            final TextView text = (TextView) dialog.findViewById(R.id.text);
+            final TextView versionView = (TextView) dialog.findViewById(R.id.version);
 			
 			email.setText(getResources().getString(R.string.email));
 			author.setText(getResources().getString(R.string.author));
 			text.setText(getResources().getString(R.string.aboutText));
 			versionView.setText("Version: " + version);
-			ImageView image = (ImageView) dialog.findViewById(R.id.image);
+
+            final ImageView image = (ImageView) dialog.findViewById(R.id.image);
 			image.setImageResource(icon);
 			dialog.setCanceledOnTouchOutside(true);
 			dialog.show();
@@ -250,32 +224,49 @@ public class IHCControl extends Activity {
 	// Called when app is resumed from background
 	@Override
 	protected void onResume() {
-		super.onResume();		
-				
-		this.autoLoginBox.setChecked(settings.getBoolean("autologin", false));
+		super.onResume();
 		
-		if(this.autoLoginBox.isChecked() && ((ApplicationContext) getApplication()).getIsAppRestarted())
+		if(settings.getBoolean("autologin", false) && ((ApplicationContext) getApplication()).getIsAppRestarted())
 		{
-			String ip;
-			String wanip = ((EditText) findViewById(R.id.wan_ip)).getText().toString();
-			String lanip = ((EditText) findViewById(R.id.lan_ip)).getText().toString();
-			if (this.wanCheckBox.isChecked()) {
-				ip = wanip;
-			} else {
-				ip = lanip;
-			}
+            String ip;
+            String wanip = "";
+            String lanip = "";
+            String username = "";
+            String password = "";
 
-			String username = ((EditText) findViewById(R.id.username)).getText().toString();
-			String password = ((EditText) findViewById(R.id.password)).getText().toString();
+            this.wanIpEditText = (EditText) findViewById(R.id.wan_ip);
 
-			SharedPreferences saveSettings = getSharedPreferences(PREFS_NAME, 0);
-			SharedPreferences.Editor editor = saveSettings.edit();
+            if(this.wanIpEditText.getText() != null)
+                wanip =  wanIpEditText.getText().toString();
+
+
+            this.lanIpEditText = (EditText) findViewById(R.id.lan_ip);
+            if(this.lanIpEditText.getText() != null)
+                lanip = lanIpEditText.getText().toString();
+
+            if (this.wanCheckBox.isChecked()) {
+                ip = wanip;
+            } else {
+                ip = lanip;
+            }
+
+
+            this.usernameEditText = (EditText) findViewById(R.id.username);
+            if(this.usernameEditText.getText() != null)
+			    username = this.usernameEditText.getText().toString();
+
+
+            this.passwordEditText = (EditText) findViewById(R.id.password);
+            if(this.passwordEditText.getText() != null)
+			    password = this.passwordEditText.getText().toString();
+
+			final SharedPreferences saveSettings = getSharedPreferences(PREFS_NAME, 0);
+			final SharedPreferences.Editor editor = saveSettings.edit();
 			editor.putString("username", username);
 			editor.putString("password", password);
 			editor.putString("lan_ip", lanip);
 			editor.putString("wan_ip", wanip);
 			editor.putString("active_ip", ip);
-			editor.putBoolean("autologin", this.autoLoginBox.isChecked());
 			editor.putBoolean("wanonly", this.wanCheckBox.isChecked());
 			editor.commit();
 
@@ -284,29 +275,6 @@ public class IHCControl extends Activity {
 		((ApplicationContext) getApplication()).setIsAppRestarted(true);
 
 	}
-	
-	private boolean connectionIsAvailable()
-	 {
-		 ConnectivityManager connMgr = (ConnectivityManager)
-		 this.getSystemService(Context.CONNECTIVITY_SERVICE);
-
-		 final android.net.NetworkInfo wifi =
-		 connMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-	
-	
-		 final android.net.NetworkInfo mobile =
-		 connMgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-	
-	
-		 if( wifi.isAvailable() ){
-		 	return true;
-		 }
-		 else if( mobile.isAvailable() ){
-		 	return true;
-		 }
-		 else
-		 	return false;
-	 }
 
 	private class LoginTask extends AsyncTask<String, Void, Boolean> {
 
@@ -332,8 +300,11 @@ public class IHCControl extends Activity {
 			} else {
 				dialog.cancel();
 				int duration = Toast.LENGTH_LONG;
-				Toast toast;
-				toast = Toast.makeText(getApplicationContext(), getResources().getString(R.string.login_failed_msg) + loginMessage, duration);
+
+                if (isFinishing())
+                    return;
+
+                final Toast toast = Toast.makeText(getApplicationContext(), getResources().getString(R.string.login_failed_msg) + loginMessage, duration);
 				toast.show();
 			}
 		}
@@ -349,8 +320,10 @@ public class IHCControl extends Activity {
 		
 		@Override
 		protected Boolean doInBackground(Boolean... params) {
-			// open the file for reading     
-			FileInputStream streamIn = null;
+			// open the file for reading
+            FileInputStream streamIn = null;
+
+            if(simulationMode) {
 				try {     
 					streamIn = new FileInputStream("/sdcard/onshuis.vis");
 					 //streamIn = new FileInputStream("/sdcard/julio.xml");
@@ -360,6 +333,7 @@ public class IHCControl extends Activity {
 					e.printStackTrace();
 					//You'll need to add proper error handling here
 				}
+            }
 
 			if(params[0] || forceReload)
 			{
@@ -367,8 +341,8 @@ public class IHCControl extends Activity {
 				if(((ApplicationContext) getApplication()).writeDataFile("iremote.data"))
 					forceReload = false;
 				
-				SharedPreferences saveSettings = getSharedPreferences(PREFS_NAME, 0);
-				SharedPreferences.Editor editor = saveSettings.edit();
+				final SharedPreferences saveSettings = getSharedPreferences(PREFS_NAME, 0);
+				final SharedPreferences.Editor editor = saveSettings.edit();
 				editor.putBoolean("reloadProject",false);
 				editor.commit();
 			}
@@ -377,7 +351,7 @@ public class IHCControl extends Activity {
 
 		protected void onPostExecute(Boolean b) {
 			dialog.cancel();
-			Intent intent = new Intent(getApplicationContext(), LocationFragment.class);
+			final Intent intent = new Intent(getApplicationContext(), LocationFragment.class);
 			startActivity(intent);
 		}
 	}
